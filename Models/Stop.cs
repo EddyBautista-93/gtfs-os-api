48014e3e628e5151150e5f699aca169d5ab5c31f
EddyBautista <bautista.j.eddy@gmail.com>
2024-07-24 21:26:19 -0500

    comment out csv headers in stops class

--- Models/Stop.cs
+++ Models/Stop.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace gtfs_api.Models
 {
-    stop_id
-    stop_code
-    stop_name
-    stop_desc
-    stop_lat
-    stop_lon
-    zone_id
-    stop_url
-    location_type
-    parent_station
-    stop_timezone
-    wheelchair_boarding
+    // stop_id
+    // stop_code
+    // stop_name
+    // stop_desc
+    // stop_lat
+    // stop_lon
+    // zone_id
+    // stop_url
+    // location_type
+    // parent_station
+    // stop_timezone
+    // wheelchair_boarding
 
     public class Stop
     {
